65bed054e4fe268ae189148febad2a25a13809db
AnGeD13 <annagetun13@gmail.com>
2025-03-08 22:10:15 +1000

    added a dark theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-root',
+  imports: [NgClass],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'signIn-form';
+export class AppComponent{
+  title = "SignInForm";
+  isWhiteTheme = true;
+
+  onChangeTheme(){
+    document.body.classList.toggle('dark-theme');
+    this.isWhiteTheme = !this.isWhiteTheme;
+  }
 }
